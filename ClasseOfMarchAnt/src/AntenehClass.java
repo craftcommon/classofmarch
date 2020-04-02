c5a1c554459bd89943d8b7c08869307fc46d15a8
chuchusara1975 <chuchuhselassie33@gmail.com>
2020-04-02 08:10:19 -0700

    hi class

---
+++ ClasseOfMarchAnt/src/AntenehClass.java
@@ -0,0 +1,13 @@
+
+public class AntenehClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("how you doing?");
+		System.out.println("how you doing?");
+		System.out.println("how you doing?");
+		
+
+	}
+
+}
