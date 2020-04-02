77f2df0aa95b94f451063ec68ffedc81bdaeacf4
netbel131 <ntirfea@gmail.com>
2020-04-01 22:25:11 -0400

    Jave day 1

--- Netsanet_project/src/Greeting_from_Netsanet.java
+++ Netsanet_project/src/Greeting_from_Netsanet.java
@@ -1,4 +1,17 @@
 
 public class Greeting_from_Netsanet {
+	
+	public static	void main(String[] args) {
+		String name = "James";
+		int salary =1200;
+		double bonus = 230.0;
+		double total_salery = salary+bonus;
+		
+		System.out.println("The name of the employee is :" +name);
+		System.out.println("The Salery of the employee is :" +salary);
+		System.out.println("The Bonus of the employee is :" +bonus);
+		System.out.println("Totyal salaryf the employee is :" +total_salery);
+			
 
+}
 }
