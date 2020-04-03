88d79cd13ae009469de30e9238f4b600a8a07c24
aklilubek <aklilubek2016@gmail.com>
2020-04-03 13:27:13 -0700

    first Exercise

---
+++ JsLab_Akmarch/src/AkLabpkg/Exr_Ak.java
@@ -0,0 +1,17 @@
+package AkLabpkg;
+
+public class Exr_Ak {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int number1=25;
+     int number2=50;
+     int result;
+     result = number1 + number2; 
+   
+   System.out.println("Number 1:"+number1);
+   System.out.println("Number 2:"+number2);
+   System.out.println("Result of Addition:"+result);
+	}
+
+}
