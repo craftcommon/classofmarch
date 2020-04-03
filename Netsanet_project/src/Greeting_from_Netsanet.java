64869864d2e3825ba52cce2fabc4c3b5fc5749f7
netbel131 <ntirfea@gmail.com>
2020-04-02 20:51:40 -0400

    Greetings from Netsanet

--- Netsanet_project/src/Greeting_from_Netsanet.java
+++ Netsanet_project/src/Greeting_from_Netsanet.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
 
 public class Greeting_from_Netsanet {
 	
 	public static	void main(String[] args) {
-		String name = "James";
+		/*String name = "James";
 		int salary =1200;
-		double bonus = 230.0;
-		double total_salery = salary+bonus;
+		double bonus = 230.0;*/
 		
-		System.out.println("The name of the employee is :" +name);
-		System.out.println("The Salery of the employee is :" +salary);
-		System.out.println("The Bonus of the employee is :" +bonus);
-		System.out.println("Totyal salaryf the employee is :" +total_salery);
-			
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		String name =in.nextLine();
+		
+		System.out.println("Please enter your salary: ");
+		Integer salary =in.nextInt();
+		
+		System.out.println("Please enter yor bonus: ");
+		double bonus =in.nextDouble();
+		
+		double total_salery = salary+bonus; 
+		System.out.println ("Total Salary: " + total_salery);
 
 }
 }
