8c2e3893778b999bda8622b4422b9636fb33ba33
sime22-Adugna <muletasime@gmail.com>
2020-04-04 15:08:37 -0500

    hellooo

--- Addisproject/src/Anan2020project4.java
+++
@@ -1,8 +0,0 @@
-
-public class Anan2020project4 {
-	public class void ("anan2020)");
-     System.out.println("hello anan");
-
-
-
-}
--- Ananproject1/src/anan2020a.java
+++ Ananproject1/src/anan2020a.java
@@ -1,10 +1,10 @@
 
-public class Addisprojectclass {
+public class anan2020a {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-"Addis project"
+        
+		Sytem.out.println("hello anan");
 	}
 
 }
