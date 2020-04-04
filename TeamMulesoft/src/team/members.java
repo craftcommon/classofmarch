afde165e390a5c4e0adff9a2015e5c28a102b8f3
Kasaye <kasayegignarta@gmail.com>
2020-04-03 22:33:22 -0500

    team Omaha

---
+++ TeamMulesoft/src/team/members.java
@@ -0,0 +1,22 @@
+package team;
+
+public class members {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String m1="Jhon, ";
+		String m2="Kas, ";
+		String m3="Gee, ";
+		String m4="Hafi, ";
+		String m5="Sib, ";
+		String m6 ="Matio";
+		
+		System.out.println(m1);
+		System.out.println(m2);
+		System.out.println(m3);
+		System.out.println(m4);
+		System.out.println(m5);
+		System.out.println(m6);
+	}
+
+}
