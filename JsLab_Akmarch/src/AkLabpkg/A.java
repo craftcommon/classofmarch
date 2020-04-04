f3f9b327867e52a223a394b32c76354c65583c15
aklilubek <aklilubek2016@gmail.com>
2020-04-03 21:06:14 -0700

    Deff_Typ_Var

---
+++ JsLab_Akmarch/src/AkLabpkg/A.java
@@ -0,0 +1,15 @@
+package AkLabpkg;
+
+public class A {
+
+	int data=50;
+	
+	static int m=100;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int n=90;
+    m=200;
+    System.out.println("The Out of m:"+m);
+	}
+
+}
