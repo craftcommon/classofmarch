cd073a9cd5784100158741e7f4f39c12497b3d83
yohannes <imyonet@gmail.com>
2020-04-03 22:24:49 -0500

    last commit

--- lastTry/src/lastTry/yoclass.java
+++ lastTry/src/lastTry/yoclass.java
@@ -1,10 +1,10 @@
 package lastTry;
 
-public class JohnClass {
+public class yoclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Joe the greatest");
+     System.out.println("yohannes");
 	}
 
 }
