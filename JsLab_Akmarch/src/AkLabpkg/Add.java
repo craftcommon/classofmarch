b61313c69ae9e8b85801b5ddddf2dd20fa407b2f
aklilubek <aklilubek2016@gmail.com>
2020-04-04 21:50:05 -0700

    Addition of the two integers

---
+++ JsLab_Akmarch/src/AkLabpkg/Add.java
@@ -0,0 +1,14 @@
+package AkLabpkg;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=10;
+       int b=10;
+       int c;
+       c=a+b;
+       System.out.println("The Total Sum is:" +c);
+	}
+
+}
