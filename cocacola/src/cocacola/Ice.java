15053638008bb67df0df4c91305480c8ae07c3b7
sib <gebremichael17@gmail.com>
2020-04-04 19:06:42 -0500

    sibhat commit

---
+++ cocacola/src/cocacola/Ice.java
@@ -0,0 +1,16 @@
+package cocacola;
+
+public class Ice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		
+		int x=7;
+        int y=6;
+	    int z= x*y;
+	 System.out.println(z);
+	
+	}
+
+}
