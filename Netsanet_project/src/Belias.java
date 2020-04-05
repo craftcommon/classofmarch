e134ec3adfac4145a011a9317684f06a435d69c6
netbel_Lab <netbel_Lab@10.0.0.186>
2020-04-05 11:06:21 -0700

    Hello

--- Netsanet_project/src/Belias.java
+++ Netsanet_project/src/Belias.java
@@ -3,7 +3,7 @@ public class Belias {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("sabontu");
+System.out.println("Hello there this is Netsnaet ");
 	}
 
 }
