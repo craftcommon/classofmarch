b1bcc6e576264ffef74076fd60a1fd40c2831dfd
regasa1 <regasa_c@yahoo.com>
2020-04-11 19:12:05 -0400

    test maths

---
+++ SecondCompany/src/Gaint.java
@@ -0,0 +1,28 @@
+
+public class Gaint {
+public static void main (String[]args){
+
+	
+int a = (int)3.678;
+ double b =(int)3.678;
+float c = 3.14f;
+int d = (int)c;
+int e =6789;
+float f= (int) e;
+long g =12345678;
+byte h =(byte)g;                 // not enough memory (data may lost)
+
+
+
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(h);
+
+
+
+}
+}
