21e3d2ce73542945cee55e98cba900e27cf21d09
yenework27 <yeneworkyalew@gmail.com>
2020-04-13 14:48:12 -0400

    Hard work

---
+++ YeneworkFirstJavaProject/src/ClassPracticeClass1.java
@@ -0,0 +1,25 @@
+public class ClassPracticeClass1 {
+	
+   String name ="Jimes";
+   int Salary =1200;
+   double bonus = 230;
+    
+   public double total_Salary() {
+	   return Salary+bonus;
+	   }
+     public void showMethod() {
+    	 System.out.println("The nameof the employee is :" + name);
+    	 System.out.println("The Salary of the employee is :" + Salary);
+    	 System.out.println("The bonus of the employee is :" + bonus);
+    	 System.out.println("The total Salary of the employee is :" + total_Salary());
+              }
+   public class practiceClass1  {
+	   
+	
+	   
+     
+     
+     
+     
+     }
+}
